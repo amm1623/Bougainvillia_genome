2e4ea960106b48e49a098a974329eb76826439ff
amm1623 <amacia16@ucsc.edu>
2025-02-05 14:10:10 -0800

    job_RepeatMasker.sh

---
+++ Synteny_and_TE/job_RepeatMasker.sh
@@ -0,0 +1,7 @@
+module load repeatmodeler
+
+RepeatMasker -lib Bougainvillia-families.fa Bougainvillia_filtered.fa -a
+
+export LANGUAGE=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
